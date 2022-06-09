ee1617ea34ea34dbd1c99121c9c21b6ce1e2c884
MoonDoorKing <rizingsniper@kakao.com>
2022-06-09 19:44:35 +0900

    addktoon

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -24,45 +23,19 @@ def home():
 def get_naver():
     return render_template('naver.html')
 
-# kakao handler start
 @app.route('/kakao_webtoon', methods=["GET"])
 def get_kakao():
     return render_template('kakao.html')
 
+@app.route('/ktoon_webtoon', methods=['GET'])
+def get_ktoon():
+    return render_template('ktoon.html')
+
 @app.route('/kakao_webtoon/post', methods=["POST"])
 def post_kakao():
     url_receive = request.form['give_url']
-    star_receive = request.form['give_star']
-    comment_receive = request.form['give_comment']
-
-    # bs4 and requests
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-
-    doc = {
-        "url": url_receive,
-        "star": star_receive,
-        "comment": comment_receive,
-        "title": title,
-        "image": image,
-    }
-    db.kakao.insert_one(doc)
-
-    return jsonify({'msg': '등록 완료!'})
-
-@app.route('/kakao_webtoon/get', methods=["GET"])
-def kakao():
-    webtoons = list(db.kakao.find({}, {'_id':False}))
-    return jsonify({'webtoons':webtoons})
-
-
-@app.route('/kakao_webtoon/recommend', methods=["GET"])
-def kakao__recommend():
-    url_receive = 'https://page.kakao.com/main?categoryUid=10&subCategoryUid=1002'
+    # star_receive = request.form['give_star']
+    # comment_receive = request.form['give_comment']
 
     # bs4 and requests
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -72,37 +45,100 @@ def kakao__recommend():
     dates = soup.select_one('#root > div.jsx-3157985592.mainContents.mainContents_pc > div.css-1sna24c > ul')
     first_toon = {
         'title' : webtoons.select_one('a:nth-child(2) > li > div > div > div > img')['alt'],
-        'url' : "https://page.kakao.com"+webtoons.select_one('a:nth-child(2)')['href'],
         'image' : "https:"+webtoons.select_one('a:nth-child(2) > li > div > div > div > img')['data-src'],
         'date' : dates.select_one('li.css-wntfxn.e1201h8a0 > div').text
     }
     second_toon = {
         'title': webtoons.select_one('a:nth-child(3) > li > div > div > div > img')['alt'],
-        'url' : "https://page.kakao.com"+webtoons.select_one('a:nth-child(3)')['href'],
         'image': "https:" + webtoons.select_one('a:nth-child(3) > li > div > div > div > img')['data-src'],
         'date': dates.select_one('li.css-wntfxn.e1201h8a0 > div').text
     }
     third_toon = {
         'title': webtoons.select_one('a:nth-child(4) > li > div > div > div > img')['alt'],
-        'url' : "https://page.kakao.com"+webtoons.select_one('a:nth-child(4)')['href'],
         'image': "https:" + webtoons.select_one('a:nth-child(4) > li > div > div > div > img')['data-src'],
         'date': dates.select_one('li.css-wntfxn.e1201h8a0 > div').text
     }
-    doc = [first_toon, second_toon, third_toon]
-    return jsonify({'msg':'추천웹툰 업데이트 완료', 'webtoons': doc})
+    db.kakao.insert_one(first_toon)
+    db.kakao.insert_one(second_toon)
+    db.kakao.insert_one(third_toon)
+    return jsonify({'msg': '등록 완료!'})
 
-# kakao handler end
+@app.route('/ktoon_comment/post', methods=['POST'])
+def comment_ktoon():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
 
-@app.route('/ktoon_webtoon', methods=["GET"])
-def get_ktoon():
-    return render_template('ktoon.html')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
 
-@app.route('/contact', methods=["GET"])
-def get_contact():
-    return render_template('contact.html')
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    image = soup.select_one('meta[property="og:image"]')['content']
+    title = soup.select_one('meta[property="og:title"]')['content']
+
+    doc = {
+        'url': url_receive,
+        'image': image,
+        'title': title,
+        'star': star_receive,
+        'comment': comment_receive
+    }
+    db.ktoon_comments.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route('/ktoon/recommend', methods=["GET"])
+def post_ktoon():
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.myktoon.com/web/webtoon/works_list.kt', headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+
+    first_webtoon = {
+        'date' : soup.select_one('#container > section > div > article.col.selected > div > h4').text,
+        'title' : soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(1) > a > div.info > strong').text,
+        'image' : soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(1) > a > div.thumb > img')['src'],
+        'url' : soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(1) > a')['href']
+    }
+    second_webtoon = {
+        'date': soup.select_one('#container > section > div > article.col.selected > div > h4').text,
+        'title': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(2) > a > div.info > strong').text,
+        'image': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(2) > a > div.thumb > img')['src'],
+        'url': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(2) > a')['href']
+    }
+    third_webtoon = {
+        'date': soup.select_one('#container > section > div > article.col.selected > div > h4').text,
+        'title': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(3) > a > div.info > strong').text,
+        'image': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(3) > a > div.thumb > img')['src'],
+        'url': soup.select_one('#container > section > div > article.col.selected > div > ul > li:nth-child(3) > a')['href']
+    }
+    # container > section > div > article.col.selected > div > ul > li:nth-child(2) > a
+    # container > section > div > article.col.selected > div > ul > li:nth-child(1) > a
+    doc = [first_webtoon,second_webtoon,third_webtoon]
+
+    # container > section > div > article.col.selected > div > h4 요일
+
+    # container > section > div > article.col.selected > div > ul > li:nth-child(1) > a > div.thumb > img 섬네일
+
+    # container > section > div > article.col.selected > div > ul > li:nth-child(1) > a > div.info > strong 제목
+    return jsonify({'webtoons':doc})
+
+@app.route('/ktoon_comment/get', methods=["GET"])
+def comment_show_ktoon():
+    comment_list = list(db.ktoon_comments.find({}, {'_id':False}))
+    return jsonify({'comment':comment_list})
+
+@app.route('/kakao_webtoon/get', methods=["GET"])
+def kakao():
+    webtoons = list(db.kakao.find({}, {'_id':False}))
+    return jsonify({'webtoons':webtoons})
+
+@app.route('/ktoon_webtoon/get', methods=["GET"])
+def ktoon():
+    webtoon_list = list(db.ktoon.find({}, {'_id':False}))
+    return jsonify({'webtoons':webtoon_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
-
-#container > section > div > article 그날
-#container > section > div > article.col.selected 평범
---
+++ static/js/ktoon.js
@@ -0,0 +1,96 @@
+function show_comment() {
+    $.ajax({
+        type: 'GET',
+        url: '/ktoon_comment/get',
+        data: {},
+        success: function (response) {
+            let rows = response['comment']
+            for (let i = 0; i < rows.length; i++) {
+                let url = rows[i]['url']
+                let image = rows[i]['image']
+                let title = rows[i]['title']
+                let star = rows[i]['star']
+                let comment = rows[i]['comment']
+
+                let star_image = '⭐'.repeat(star)
+
+                let temp_html = `<article class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                                            <div class="service-item rounded overflow-hidden">
+                                                <!-- 웹툰 이미지 src에 넣기-->
+                                                <img id="card-image" class="img-fluid" src=${image} alt="">
+                                                <div class="position-relative p-4">
+                                                    <h4 id="card-title" class="mt-2">${title}</h4>
+                                                    <p><span id="card-comment">${comment}</span> - <span id="card-author">작성자</span></p>
+                                                    <p id="card-star">${star_image}</p>
+                                                    <!-- 웹툰 URL href에 넣기 -->
+                                                    <a id="card-url" class="small fw-medium" href="${url}">웹툰으로 이동<i class="fa fa-arrow-right ms-2"></i></a>
+                                                </div>
+                                            </div>
+                                        </article>`
+
+                $('#poster').append(temp_html)
+            }
+        }
+    })
+}
+$(document).ready(function () {
+    show_comment();
+    show_webtoon();
+});
+function show_webtoon() {
+    $.ajax({
+        type: 'GET',
+        url: '/ktoon/recommend',
+        data: {},
+        success: function (response) {
+            let rows = response['webtoons']
+            for(let i = 0; i<rows.length;i++)
+            {
+                let title = rows[i]['title']
+                let image = rows[i]['image']
+                let date = rows[i]['date']
+                let url = rows[i]['url']
+                let temp_html = `<div class="col-lg-4 col-md-6 portfolio-item third">
+                                    <div class="portfolio-img rounded overflow-hidden">
+                                        <img class="img-fluid" src=${image} alt="">
+                                        <div class="portfolio-btn">
+                                            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=${url}><i
+                                                    class="fa fa-link"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="pt-3">
+                                        <p class="rec-day text-primary mb-0">${date}요일 연재</p>
+                                        <hr class="text-primary w-25 my-2">
+                                        <h5 class="lh-base">${title}</h5>
+                                    </div>
+                                </div>`
+
+                $('#webtoon-box').append(temp_html)
+            }
+        }
+    })
+}
+
+function save_comment() {
+    let url = $('#url').val()
+    let star = $('#star').val()
+    let comment = $('#comment').val()
+
+    $.ajax({
+        type: 'POST',
+        url: '/ktoon_comment/post',
+        data: {url_give: url, star_give: star, comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+
+function open_box() {
+    $('#post-box').show()
+}
+function close_box() {
+    $('#post-box').hide()
+}
