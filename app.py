835f0c7af3bbbaf42ac255c9c32dd5f522ba7580
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-06-09 20:10:07 +0900

    Removed text and Modified app.py

--- app.py
+++ app.py
@@ -164,6 +164,3 @@ def get_contact():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
-
-#container > section > div > article 그날
-#container > section > div > article.col.selected 평범
